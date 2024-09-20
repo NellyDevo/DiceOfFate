f79920bfce7b188bc8f6954a352e58d356206c88
NellyDevo <Noelle.Devonshire@yahoo.com>
2024-09-19 21:46:01 -0400

    localize reward

--- src/main/java/diceoffate/reward/DiceReward.java
+++ src/main/java/diceoffate/reward/DiceReward.java
@@ -3,15 +3,18 @@ package diceoffate.reward;
 import basemod.abstracts.CustomReward;
 import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import diceoffate.helpers.DiceManager;
 import diceoffate.helpers.DiceTexture;
 
 public class DiceReward extends CustomReward {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("diceoffate:reward");
     public int amount;
 
     public DiceReward(int amount) {
-        super((Texture)null, amount + " Dice Of Fate", RewardType.DICE_OF_FATE_REWARD);
+        super((Texture)null, amount + (amount == 1 ? uiStrings.TEXT[0] : uiStrings.TEXT[1]), RewardType.DICE_OF_FATE_REWARD);
         this.amount = amount;
         iconRegion = DiceTexture.getDiceImage();
     }
